ee1b22efcd9b3bdfd1d81cc5b8c684d83d416d0d
David Cook <divergentdave@gmail.com>
2019-05-09 21:30:58 -0500

    Println debugging

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ extern crate sled;
 
 fn main() {
     let config = sled::ConfigBuilder::default().temporary(true).build();
+    println!("Setting up tree");
     let tree = sled::Db::start(config).unwrap();
     let key: [u8; 1] = [1];
+    println!("Doing first read on empty tree");
     let result = tree.get(&key).unwrap();
     assert!(result == None);
+    println!("Setting value");
     tree.set(&key, Vec::new()).unwrap();
+    println!("Reading back value");
     tree.get(&key).unwrap().unwrap();
+    println!("Done!");
 }
